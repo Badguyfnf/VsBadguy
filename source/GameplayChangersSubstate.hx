c7ee31937c054f02f553735f17909bd387f20fa3
Badguyfnf <116070144+Badguyfnf@users.noreply.github.com>
2023-07-24 00:17:10 +0300

    Update GameplayChangersSubstate.hx

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -93,8 +93,8 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 		var option:GameplayOption = new GameplayOption('Practice Mode', 'practice', 'bool', false);
 		optionsArray.push(option);
 
-		var option:GameplayOption = new GameplayOption('Botplay', 'botplay', 'bool', false);
-		optionsArray.push(option);
+		/*var option:GameplayOption = new GameplayOption('Botplay', 'botplay', 'bool', false);
+		optionsArray.push(option);*/
 	}
 
 	public function getOptionByName(name:String)
